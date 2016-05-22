5fd65f701c54c4ce48b40e4369ffb4460303932b
rajkumar <rajkumar.gootam@gmail.com>
2016-05-23 00:14:06 +0530

    Created ng-view in Index.html and Created ngRoute in app.js. In controller created new controller

---
+++ app/js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+ /* App Module */
+
+var phonecat = angular.module('phonecatApp', [
+  'ngRoute','phonecatControllers'
+]);
+
+phonecat.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
