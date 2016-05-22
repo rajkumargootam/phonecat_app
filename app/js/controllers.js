a7b017039cfd9829a5af86d0a5a459c276126eba
rajkumar <rajkumar.gootam@gmail.com>
2016-05-23 00:39:39 +0530

    Using  getting data and created view in Phone-detail.html

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,14 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('phones/phones.json').success(function(data) {
       $scope.phones = data;
-  });
+   });
 
    $scope.orderProp = 'age';
 }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-  }]);
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+ }]);
